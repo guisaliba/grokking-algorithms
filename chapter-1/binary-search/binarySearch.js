583a2f4f66a92f74bd6bab8ff6522b8a0583e2b9
Guilherme Saliba <salibagui19@gmail.com>
2023-12-08 15:07:13 -0300

    fix: chapter 1 binary search algorithm

--- chapter-1/binary-search/binarySearch.js
+++ chapter-1/binary-search/binarySearch.js
@@ -1,12 +1,12 @@
 function binarySearch(list, item) {
   let low = 0;
-  let high = list.length - 1;
+  let high = list.length;
 
   while (low <= high) {
     let middle = Math.floor((low + high) / 2);
     let guess = list[middle];
 
-    if (guess == item) {
+    if (guess === item) {
       return middle;
     } else if (guess < item) {
       low = middle + 1;
@@ -18,5 +18,5 @@ function binarySearch(list, item) {
   return false;
 }
 
-const list = [1, 3, 5, 7, 9];
-binarySearch(list, 3); // 1
+const list = [1, 3, 5, 7, 9, 10, 11, 12, 15, 17, 19, 21, 23, 25, 42];
+console.log(binarySearch(list, 11)); // 3
